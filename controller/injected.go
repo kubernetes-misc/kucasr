fd679c3e18000abe919f6a3484287005cc331d04
Just1689 <Just1689@gmail.com>
2020-02-11 19:18:24 +0200

    better implementation of injected certs

--- controller/injected.go
+++ controller/injected.go
@@ -1,140 +1,110 @@
 package controller
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/kubernetes-misc/kudecs/client"
 	"github.com/kubernetes-misc/kudecs/model"
+	"github.com/sirupsen/logrus"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func reconcileInjected(cs model.KudecsV1) {
 
-	/////////////////////////////////////////////////
-	/*
-		WHAT NEEDS TO BE DONE
-	*/
-	/////////////////////////////////////////////////
-	//create, update, merge := getInjectedSecretTasks(cs)
-	//logrus.Debugln(fmt.Sprintf("getInjectedSecretTasks returning create: %v, update: %v, merge: %v", len(create), len(update), len(merge)))
-	//inject := make(map[string]*v1.Secret)
+	create, update := getInjectedSecretTasks(cs)
+	logrus.Debugln(fmt.Sprintf("getInjectedSecretTasks returning create: %v, update: %v", len(create), len(update)))
 
-	/////////////////////////////////////////////////
-	/*
-		SOURCE OF MASTER SECRET
-	*/
-	/////////////////////////////////////////////////
-	//for _, c := range create {
-	//	masterSecret, err := client.GetSecret(model.StoreNamespace, cs.GetMasterSecretName())
-	//	if err != nil {
-	//		logrus.Errorln(fmt.Sprintf("unexpected error getting master secret (%s/%s) during create of injected secret", model.StoreNamespace, cs.GetMasterSecretName()))
-	//		continue
-	//	}
-	//
-	//	k := masterSecret.Data[c]
-	//	se := &cv1.Secret{
-	//		ObjectMeta: metav1.ObjectMeta{
-	//			Name: i.SecretName,
-	//			Labels: map[string]string{
-	//				model.ExpiresLabel: masterSecret.Labels[model.ExpiresLabel],
-	//			},
-	//		},
-	//		Data: map[string][]byte{
-	//			dataKey: k,
-	//		},
-	//		Type: cv1.SecretTypeOpaque,
-	//	}
-	//}
-	//var masterSecret *cv1.Secret
-	//if !create {
-	//	var err error
-	//	masterSecret, err = client.GetSecret(model.StoreNamespace, cs.GetMasterSecretName())
-	//	if err != nil {
-	//		logrus.Errorln("unexpected error! could not get master secret when create == false. Should not happen. Skipping")
-	//		return
-	//	}
-	//}
-	//
-	///////////////////////////////////////////////////
-	///*
-	//	MODIFYING THE SECRET
-	//*/
-	///////////////////////////////////////////////////
-	//if create {
-	//	logrus.Infoln("> Generating master certificate")
-	//	logrus.Infoln(fmt.Sprintf("  Requester: %s/%s", cs.Metadata.Namespace, cs.Metadata.Name))
-	//	logrus.Infoln(fmt.Sprintf("  Master stored as: %s/%s", model.StoreNamespace, cs.GetMasterSecretName()))
-	//	masterSecret = newMasterSecret(cs)
-	//}
-	//if update {
-	//	logrus.Infoln("> Updating master certificate")
-	//	logrus.Infoln(fmt.Sprintf("  Requester: %s/%s", cs.Metadata.Namespace, cs.Metadata.Name))
-	//	logrus.Infoln(fmt.Sprintf("  Master stored as: %s/%s", model.StoreNamespace, cs.GetMasterSecretName()))
-	//	updateMasterSecret(cs, masterSecret)
-	//}
-	//
-	///////////////////////////////////////////////////
-	///*
-	//	PERSISTING THE SECRET
-	//*/
-	///////////////////////////////////////////////////
-	//if create {
-	//	err := client.CreateSecret(model.StoreNamespace, masterSecret)
-	//	if err != nil {
-	//		logrus.Errorln(model.LogFAIL)
-	//		logrus.Errorln(fmt.Sprintf("  could not generate master certificate: %s/%s", model.StoreNamespace, masterSecret.Name))
-	//		return
-	//	}
-	//	logrus.Infoln(model.LogOK)
-	//	return
-	//}
-	//
-	//if update {
-	//	err := client.UpdateSecret(model.StoreNamespace, masterSecret)
-	//	if err != nil {
-	//		logrus.Errorln(model.LogFAIL)
-	//		logrus.Errorln(fmt.Sprintf("  could not update master certificate: %s/%s", model.StoreNamespace, masterSecret.Name))
-	//		return
-	//	}
-	//	logrus.Infoln(model.LogOK)
-	//	return
-	//
-	//}
+	masterSecret, err := client.GetSecret(model.StoreNamespace, cs.GetMasterSecretName())
+	if err != nil {
+		logrus.Errorln(fmt.Sprintf("unexpected error getting master secret (%s/%s) during create of injected secret", model.StoreNamespace, cs.GetMasterSecretName()))
+		return
+	}
+
+	for _, c := range create {
+		logrus.Infoln("> Creating injected certificate")
+		logrus.Infoln(fmt.Sprintf("  Requester: %s/%s", cs.Metadata.Namespace, cs.Metadata.Name))
+		logrus.Infoln(fmt.Sprintf("  Master stored as: %s/%s", model.StoreNamespace, cs.GetMasterSecretName()))
+		logrus.Infoln(fmt.Sprintf("  Secret to be created: %s/%s", c.Namespace, c.SecretName))
+
+		k := masterSecret.Data[c.SourceKey]
+		s := &v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: c.SecretName,
+				Labels: map[string]string{
+					model.ExpiresLabel + "-" + c.KeyName: masterSecret.Labels[model.ExpiresLabel],
+				},
+			},
+			Data: map[string][]byte{
+				c.KeyName: k,
+			},
+			Type: v1.SecretTypeOpaque,
+		}
+
+		err = client.CreateSecret(c.Namespace, s)
+		if err != nil {
+			logrus.Errorln(fmt.Sprintf("unexpected error creating injected secret (%s/%s)", c.Namespace, c.SecretName))
+			logrus.Infoln(model.LogFAIL)
+			continue
+		}
+		logrus.Infoln(model.LogOK)
+
+	}
+	for _, c := range update {
+		logrus.Infoln("> Updating injected certificate")
+		logrus.Infoln(fmt.Sprintf("  Requester: %s/%s", cs.Metadata.Namespace, cs.Metadata.Name))
+		logrus.Infoln(fmt.Sprintf("  Master stored as: %s/%s", model.StoreNamespace, cs.GetMasterSecretName()))
+		logrus.Infoln(fmt.Sprintf("  Secret to be updated: %s/%s", c.Namespace, c.SecretName))
+		s, err := client.GetSecret(c.Namespace, c.SecretName)
+		if err != nil {
+			logrus.Errorln(fmt.Sprintf("unexpected error getting injected secret (%s/%s)", c.Namespace, c.SecretName))
+			logrus.Infoln(model.LogFAIL)
+			continue
+		}
+		s.Labels[model.ExpiresLabel+"-"+c.KeyName] = masterSecret.Labels[model.ExpiresLabel]
+		k := masterSecret.Data[c.SourceKey]
+		s.Data[c.KeyName] = k
+		err = client.UpdateSecret(c.Namespace, s)
+		if err != nil {
+			logrus.Errorln(fmt.Sprintf("unexpected error updating injected secret (%s/%s)", c.Namespace, c.SecretName))
+			logrus.Errorln(err)
+			logrus.Infoln(model.LogFAIL)
+			continue
+		}
+		logrus.Infoln(model.LogOK)
+	}
 
 }
 
-//func getInjectedSecretTasks(cs model.KudecsV1) (create []model.InjectedSecretsV1, update []model.InjectedSecretsV1, merge []model.InjectedSecretsV1) {
-//	masterSecret, err := client.GetSecret(model.StoreNamespace, cs.GetMasterSecretName())
-//	if err != nil || masterSecret == nil {
-//		logrus.Errorln(fmt.Sprintf("unexpected error. Master secret (%s/%s) should exist. Skipping injected", model.StoreNamespace, cs.GetMasterSecretName()))
-//		return
-//	}
-//
-//	for _, i := range cs.Spec.InjectPrivateNamespace {
-//		s, err := client.GetSecret(i.Namespace, i.SecretName)
-//		if err != nil || s == nil {
-//			create = append(create, i)
-//			continue
-//		}
-//		if !certsEqual(masterSecret, s, model.DefaultPrivate) {
-//			update = append(update, i)
-//		}
-//	}
-//	for _, i := range cs.Spec.InjectPublicNamespace {
-//		s, err := client.GetSecret(i.Namespace, i.SecretName)
-//		if err != nil || s == nil {
-//			create = append(create, i)
-//			continue
-//		}
-//		if !certsEqual(masterSecret, s, model.DefaultPublic) {
-//			update = append(update, i)
-//		}
-//	}
-//	return
-//}
-//
-//func certsEqual(master, secret *v1.Secret, dataKey string) bool {
-//	if master.Labels[model.ExpiresLabel] != secret.Labels[model.ExpiresLabel] {
-//		return false
-//	}
-//	if !bytes.Equal(master.Data[dataKey], secret.Data[dataKey]) {
-//		return false
-//	}
-//	return true
-//}
+func getInjectedSecretTasks(cs model.KudecsV1) (create []model.InjectedSecretsV1, update []model.InjectedSecretsV1) {
+	masterSecret, err := client.GetSecret(model.StoreNamespace, cs.GetMasterSecretName())
+	if err != nil || masterSecret == nil {
+		logrus.Errorln(fmt.Sprintf("unexpected error. Master secret (%s/%s) should exist. Skipping injected", model.StoreNamespace, cs.GetMasterSecretName()))
+		return
+	}
+
+	for _, i := range cs.Spec.InjectedSecrets {
+		s, err := client.GetSecret(i.Namespace, i.SecretName)
+		if err != nil || s == nil {
+			logrus.Infoln(fmt.Sprintf("going need to create injected cert %s/%s", i.Namespace, i.SecretName))
+			create = append(create, i)
+			continue
+		}
+		if !certsEqual(masterSecret, s, i) {
+			logrus.Infoln(fmt.Sprintf("going need to update injected cert %s/%s", i.Namespace, i.SecretName))
+			update = append(update, i)
+		}
+	}
+
+	return
+}
+
+func certsEqual(master, secret *v1.Secret, is model.InjectedSecretsV1) bool {
+	if master.Labels[model.ExpiresLabel] != secret.Labels[model.ExpiresLabel+"-"+is.KeyName] {
+		return false
+	}
+	if !bytes.Equal(master.Data[is.SourceKey], secret.Data[is.KeyName]) {
+		return false
+	}
+	return true
+}
--- controller/recon.go
+++ controller/recon.go
@@ -1,13 +1,8 @@
 package controller
 
 import (
-	"bytes"
-	"fmt"
-	"github.com/kubernetes-misc/kudecs/client"
 	"github.com/kubernetes-misc/kudecs/model"
 	"github.com/sirupsen/logrus"
-	cv1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var ReconHub = NewReconHub()
@@ -33,85 +28,5 @@ func (r *reconHub) Add(cs model.KudecsV1) {
 
 func checkAndUpdate(cs model.KudecsV1) {
 	reconcileMaster(cs)
-	checkInjected(cs)
-}
-
-func checkInjected(cs model.KudecsV1) {
-	//Get the master
-	masterSecret, err := client.GetSecret(model.StoreNamespace, cs.GetMasterSecretName())
-	if err != nil || masterSecret == nil {
-		logrus.Errorln("checkInjected could not find the secret!")
-		return
-	}
-
-	//Delete broken / out of data secrets
-	for _, i := range cs.Spec.InjectedSecrets {
-		deleteIfWrong(i, masterSecret)
-	}
-
-	//Create missing secrets
-	for _, i := range cs.Spec.InjectedSecrets {
-		createSecretFromMaster(i, masterSecret)
-	}
-
-}
-
-func createSecretFromMaster(i model.InjectedSecretsV1, masterSecret *cv1.Secret) {
-	//Escape if secret exists as we checked that in deleteIfWrong()
-	if s, err := client.GetSecret(i.Namespace, i.SecretName); err == nil && s != nil {
-		return
-	}
-	logrus.Println(fmt.Sprintf("> Injecting secret %s/%s", i.Namespace, i.SecretName))
-	logrus.Println(fmt.Sprintf("  Master secret: %s/%s", model.StoreNamespace, masterSecret.Name))
-	logrus.Println(fmt.Sprintf("  K8s object: %s/%s", i.Namespace, i.SecretName))
-	logrus.Println(fmt.Sprintf("  Injected from: %s", i.SourceKey))
-	logrus.Println(fmt.Sprintf("  Injected as: %s", i.KeyName))
-
-	k := masterSecret.Data[i.SourceKey]
-	se := &cv1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: i.SecretName,
-			Labels: map[string]string{
-				model.ExpiresLabel: masterSecret.Labels[model.ExpiresLabel],
-			},
-		},
-		Data: map[string][]byte{
-			i.KeyName: k,
-		},
-		Type: cv1.SecretTypeOpaque,
-	}
-
-	err := client.CreateSecret(i.Namespace, se)
-	if err != nil {
-		logrus.Errorln(err)
-		logrus.Errorln("failed to store injected secret as ", model.StoreNamespace, se.Name)
-		return
-	}
-	logrus.Println(model.LogOK)
-
-}
-
-func deleteIfWrong(i model.InjectedSecretsV1, masterSecret *cv1.Secret) {
-	secret, err := client.GetSecret(i.Namespace, i.SecretName)
-	if err != nil || secret == nil {
-		logrus.Debugln("secret ", i.Namespace, i.SecretName, "does not exist... and that is ok")
-		return
-	}
-	mustDelete := false
-
-	//Check if the secrets have different expiry dates
-	mustDelete = mustDelete || secret.Labels[model.ExpiresLabel] != masterSecret.Labels[model.ExpiresLabel]
-
-	//Check that private or public secret is correct
-	mustDelete = mustDelete || !bytes.Equal(secret.Data[i.KeyName], masterSecret.Data[i.SourceKey])
-
-	logrus.Debugln("Should we delete the injected secret? ", mustDelete, i.SecretName)
-
-	if mustDelete {
-		logrus.Println(fmt.Sprintf("> Deleting secret %s/%s", i.Namespace, i.SecretName))
-		err := client.DeleteSecret(i.Namespace, i.SecretName)
-		if err != nil {
-			logrus.Errorln("  failed to delete secret ", i.Namespace, i.SecretName)
-		}
-	}
+	reconcileInjected(cs)
 }
